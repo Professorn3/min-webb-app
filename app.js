f405ddc735d9a22f986598b8a247eb2e6bc1d65a
Ak S <ak@MacBookAir.local>
2026-03-01 12:50:49 +0100

    Första inlämningen: Hello World

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 80;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World! Denna sida körs på min Contabo VPS via ett deploy-skript.</h1>');
+});
+
+// Viktigt: Lyssna på 0.0.0.0 för att ta emot trafik utifrån
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Webbservern är igång på port ${port}`);
+});
